14ff65a7086d4eec1f1febc0177824d76072f617
Antaligent <100718722+Antaligent@users.noreply.github.com>
2023-11-17 09:18:42 +0100

    3

--- code.js
+++ code.js
@@ -4,26 +4,24 @@ let cart = {};
 let cart2 = {};
 
 window.onload = async () => {
-  const wizards = await getAllWizards();
+  //const wizards = await getAllWizards();
   const houses = await getAllhouses();
   const merda = "funciona api";
 
   const spinnerHtmlElement = document.getElementById('spinner');
   spinnerHtmlElement.remove();
 
-  for (const wizard of wizards) {
+  /*for (const wizard of wizards) {
     const mainHtmlElement = document.getElementById('main');
     const newElement = document.createElement('button');
     newElement.innerHTML = `
-      <h2>${wizard.FirstName}</h2>
+      <h2>${wizard.id}</h2>
       <p>${wizard.LastName}</p>
       <p>${merda}</p>
-      
-
     `;
     
     mainHtmlElement.appendChild(newElement);
-  }
+  }*/
 
   
   for (const house of houses) {
@@ -47,5 +45,7 @@ async function getAllWizards() {
   async function getAllhouses() {
     const response = await fetch(`${SWAPI_BASE_URL}/Houses`);
     const data = await response.json();
-    return data.results;
+    return data.id;
   }
+
+ 
